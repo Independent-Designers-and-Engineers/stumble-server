1f1fdc39d833d4a0e72d03bf708062656d15562a
Adheesh Wadkar <apwadkar@asu.edu>
2019-09-14 20:52:24 -0700

    Fixed ES6 import/export errors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Import the packages we need
-import express from "express";          // This is the main one, it'll handle running the HTTP server and capturing all requests
-import { json } from "body-parser";     // This one is used with Express so that it can read the request bodies sent to it
+const express = require("express");             // This is the main one, it'll handle running the HTTP server and capturing all requests
+const bparser = require("body-parser");         // This one is used with Express so that it can read the request bodies sent to it
 
 // Import our own modules/files
-import UserRoute from "./routes/user";  // This is the Express router for the "/user" route
+const UserRouter = require("./routes/user");    // This is the Express router for the "/user" route
 
 // If we're running the server locally, configure the environment variables from .env file
 if (process.env.NODE_ENV === "dev") {
@@ -15,10 +15,10 @@ const EXPRESS_PORT = process.env.EXPRESS_PORT || 5000;
 
 // Create the server instance and make it use the JSON parser from body-parser
 const app = express();
-app.use(json());
+app.use(bparser.json());
 
 // Set base routes to respective routers
-app.use("/user", UserRoute);    // Make "/user" the base route for the "/user" router
+app.use("/user", UserRouter);   // Make "/user" the base route for the "/user" router
 
 // If we get a request for the root, just send a simple JSON message
 app.get("/", (req, res) => {
@@ -29,3 +29,4 @@ app.get("/", (req, res) => {
 
 // Start the server listening at port specified port
 app.listen(EXPRESS_PORT);
+console.log(`Listening at ${EXPRESS_PORT}`);
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,13 @@
 // Imports
-import { Router } from "express";
+const Router = require("express").Router;
 
 // Create a new Express router for "/user" route
 const router = Router();
 
 // POST /user
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
     // TODO: Implement this (#5)
-    // If we get a POST request for "/users", just get the firstname and send it back
+    // If we get a POST request for "/user", just get the firstname and send it back
     const firstName = req.body.firstName;
     res.send(firstName + " is logged in");
 });
@@ -18,4 +18,4 @@ router.post("/create", (req, res) => {
 });
 
 // Export this so it can be used outside
-export default router;
+module.exports = router;
