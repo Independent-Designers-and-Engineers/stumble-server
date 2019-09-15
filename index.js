d8c706d99ba73d797ab2a62b002b729169d4f3c8
Adheesh Wadkar <apwadkar@asu.edu>
2019-09-14 21:27:19 -0700

    Fixed Heroku support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// Import the packages we need
+// Imports
 const express = require("express");             // This is the main one, it'll handle running the HTTP server and capturing all requests
 const bparser = require("body-parser");         // This one is used with Express so that it can read the request bodies sent to it
+const WebSocket = require("ws");
 
 // Import our own modules/files
 const UserRouter = require("./routes/user");    // This is the Express router for the "/user" route
@@ -9,13 +10,14 @@ const UserRouter = require("./routes/user");    // This is the Express router fo
 if (process.env.NODE_ENV === "dev") {
     require("dotenv").config();
 }
-const EXPRESS_PORT = process.env.EXPRESS_PORT || 5000;
-// const WS_PORT = process.env.WS_PORT || 5001;
+const PORT = process.env.PORT || 5000;
 // const MONGODB_URL = process.env.MONGODB_URL || "";
 
 // Create the server instance and make it use the JSON parser from body-parser
 const app = express();
 app.use(bparser.json());
+
+const wss = new WebSocket.Server({ server: app });
 
 // Set base routes to respective routers
 app.use("/user", UserRouter);   // Make "/user" the base route for the "/user" router
@@ -27,6 +29,7 @@ app.get("/", (req, res) => {
     });
 });
 
-// Start the server listening at port specified port
-app.listen(EXPRESS_PORT);
-console.log(`Listening at ${EXPRESS_PORT}`);
+// Start both WebSocket and Express servers listening at specified port
+app.listen(PORT);
+
+console.log(`Listening at ${PORT}`);
