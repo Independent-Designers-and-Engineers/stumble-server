ad54afa9cf0128cf0601200cbfc4c538c7aa5845
Adheesh Wadkar <apwadkar@asu.edu>
2019-09-16 18:21:12 -0700

    Added account creation

--- index.js
+++ index.js
@@ -2,22 +2,26 @@
 const express = require("express");             // This is the main one, it'll handle running the HTTP server and capturing all requests
 const bparser = require("body-parser");         // This one is used with Express so that it can read the request bodies sent to it
 const WebSocket = require("ws");
+const mongoose = require("mongoose");
 
 // Import our own modules/files
 const UserRouter = require("./routes/user");    // This is the Express router for the "/user" route
 
 // If we're running the server locally, configure the environment variables from .env file
-if (process.env.NODE_ENV === "dev") {
+if (process.env.NODE_ENV === "dev" || process.argv[2] === "dev") {
     require("dotenv").config();
 }
 const PORT = process.env.PORT || 5000;
-// const MONGODB_URL = process.env.MONGODB_URL || "";
+const MONGODB_URL = process.env.MONGODB_URL || "";
 
 // Create the server instance and make it use the JSON parser from body-parser
 const app = express();
 app.use(bparser.json());
 
 const wss = new WebSocket.Server({ server: app });
+
+// Connect to the MongoDB server
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
 
 // Set base routes to respective routers
 app.use("/user", UserRouter);   // Make "/user" the base route for the "/user" router
---
+++ models/User.js
@@ -0,0 +1,13 @@
+// Imports
+const mongoose = require("mongoose");
+
+// Define the User model with each field
+const UserSchema = new mongoose.Schema({
+    name: String,
+    phoneNumber: String,
+    password: String,
+    dateOfBirth: Date
+});
+
+// Export the model
+module.exports = mongoose.model("User", UserSchema);
