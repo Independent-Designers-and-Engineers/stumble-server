da87c2b0813bff11eefaf550a771bea28ed9982c
Adheesh Wadkar <apwadkar@asu.edu>
2019-10-04 17:20:21 -0700

    Fixed style errors and passport configuraiton

--- config/passport.js
+++ config/passport.js
@@ -2,12 +2,11 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 
-const User = mongoose.model("users");
-const keys = require("../config/keys");
+const User = require("../models/User");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secret;
+opts.secretOrKey = process.env.SECRET;
 
 module.exports = (passport) => {
     passport.use(new JwtStrategy(opts, (payload, done) => {
