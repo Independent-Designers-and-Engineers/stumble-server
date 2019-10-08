85b8466be5bb2812cb402f8d0e4bd609f3b9e68d
Jason Xu <jasonxu1@asu.edu>
2019-10-07 19:29:08 -0700

    Updated get profile

--- routes/user.js
+++ routes/user.js
@@ -89,9 +89,9 @@ router.get("/current", passport.authenticate("jwt", { session: false }), (req, r
 });
 
 // GET /user/{id}/profile
-router.get("/:id/profile", passport.authenticate("jwt", { session: false }), {password: 0}, {phoneNumber: 0}, (req, res) => {
+router.get("/:id/profile", passport.authenticate("jwt", { session: false }), (req, res) => {
     const phoneNumber = req.params["id"];
-    User.findOne({ phoneNumber: phoneNumber }, (err, user) => {
+    User.findOne({ phoneNumber: phoneNumber }, {password: 0}, {phoneNumber: 0}, (err, user) => {
         if (user) {
             let foundUser = user;
             res.status(200).send(foundUser);
